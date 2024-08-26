25f38b4522c621178445ed8c236918ac39aece39
Karhdo <dotrongkhanh.dev@gmail.com>
2024-08-26 23:07:45 +0700

    feat(terminal): Config wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,18 @@
+-- Pull in the wezterm API.
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.font = wezterm.font('Hack Nerd Font')
+config.font_size = 18
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.color_scheme = 'Tokyo Night'
+
+config.window_background_opacity = 0.5
+config.macos_window_background_blur = 10
+
+return config
